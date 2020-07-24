bc70bc72cd60b4af29c98ae9312ab7c7b3c8bed6
Edison Florez <e.florez@massey.ac.nz>
2020-07-24 12:54:26 +1200

    Reading XYZ file with Pandas

--- get_all_distances.py
+++ get_all_distances.py
@@ -1,5 +1,14 @@
 #!/usr/bin/python3.6
 
+# ------------------------------------------
+# July 2020
+#               edisonffh@gmail.com
+#
+# python3.6 script to compute bond length
+# frecuency in Hg(H_2O)_n clusters
+#
+# ------------------------------------------
+
 # -----------------------------------
 # ------ moules
 # -----------------------------------
@@ -17,10 +26,32 @@ from matplotlib import rc
 rc('text', usetex=True)
 
 # -----------------------------------
-# ------ script
+# ------ body
 # -----------------------------------
-# - reading files & creating variables
-list_xyz = []  # array for xyz files
+
+# - list for xyz files
+repited_list_xyz = []  # repited files (if any)
+list_xyz = []  # unique files
+
+# - reading files
 for input_xyz in glob.glob('*.xyz'):
-    fname = input_xyz[:-4]  # deleting file extention
-    list_xyz.append(input_xyz)  # creating an array for all xyz files
+    name_xyz = input_xyz[:-4]  # deleting file extention
+    repited_list_xyz.append(input_xyz)  # creating an array for all xyz files
+
+    # keeping unique xyz files
+    for unique_input_xyz in repited_list_xyz:
+        if unique_input_xyz not in list_xyz:
+            list_xyz.append(unique_input_xyz)
+
+list_xyz = ["w6s1.xyz"]
+
+# - reading coordinates fro XYZ file
+for file_xyz in list_xyz:
+    atoms = pd.read_csv(file_xyz, nrows=1, header=None)
+    # importing data with pandas (skipping comments line; rsecond row)
+    data_xyz = pd.read_csv(file_xyz, delim_whitespace=True,
+                           skiprows=2, header=None)
+
+    # print(f'\n file: {file_xyz}\n atoms: {int(atoms.iloc[0])}')
+    # print(f'Pandas data is: \n{data_xyz.iloc[0,:]}\n')
+    # print(f'Pandas data is: \n{data_xyz.iloc[1,:]}\n')
