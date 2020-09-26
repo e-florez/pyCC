06238a88f3a0873c3805db9c24ca97bc94a3bb71
Edison Florez <edisonffh@gmail.com>
2020-09-26 15:37:17 +1200

    Description for the atom transfer function

--- atoms_transfer.py
+++ atoms_transfer.py
@@ -19,6 +19,24 @@ import numpy as np
 
 def atom_transfer(transfer_list, header_distance_matrix, data_xyz, distance_matrix):
     """
+    *** September 2020 by particula94h@gmail.com ***
+
+    python3.x function to analyse atom transfer according to Stern-Limbach
+        see DOI: 10.1560/IJC.49.2.199 or DOI: 10.1098/rsif.2013.0518
+
+    for a list of three atoms A - X - B
+    r1 = r[AX] and r2 = r[XB]
+
+    this function compute q1 = 0.5 * (r1 - r2) and q2 = r1 + r2
+
+    Args:
+        transfer_list ([list, str]): List of three atoms A-X-B (order matters!) to make the analysis
+        header_distance_matrix (list, str): List of atoms for the distance matrix
+        data_xyz (Pandas data frame): data frame with the symbols and XYZ coordinates for each XYZ file
+        distance_matrix (numpy matrix, float): distance matrix
+
+    Returns:
+        natural_bond_coordinates [(q1, q2) type: floats]:  list of tuples q1, q2 (floats)
     """
 
     max_distance = 2.0
