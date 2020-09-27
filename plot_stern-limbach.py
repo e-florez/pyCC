97061ee08a3eca4514e9ca89e0463a67f7f5a643
Edison Florez <edisonffh@gmail.com>
2020-09-28 01:54:27 +1300

    Ca2+ and Mg2+ cluster to compare the Stern-Limbach analysis

--- plot_stern-limbach.py
+++ plot_stern-limbach.py
@@ -9,12 +9,13 @@ import numpy as np
 import sys # to get System-specific parameters
 import os # contains some useful functions on pathnames
 import glob # - Unix style pathname pattern expansion
-from scipy.interpolate import make_interp_spline, BSpline # - to smooth out your data
+from scipy.interpolate import interp1d
+from itertools import cycle # - lines style in a for loop
 
 # ---------------------------------------------------------------------------------------------------------
 # ------ body
 # ---------------------------------------------------------------------------------------------------------
-print(f'\nPlotting Radial Distribution Analisys (RDA)\n')
+print(f'\nPlotting Radial Distribution Analisys (pfile)\n')
 
 # -----------------------------------------------------------
 # - working directory
@@ -25,7 +26,7 @@ print(f"\nCurrent working directory: {os.getcwd()}")
 ##### input_arguments =
 
 # if len(sys.argv) <= 1:
-#     tmp_dir =  input(f'\nDirectory (whit the XYZ files) to make the RDA [default: empty]: ')
+#     tmp_dir =  input(f'\nDirectory (whit the XYZ files) to make the pfile [default: empty]: ')
 #     tmp_dir = tmp_dir.strip()
 
 #     if tmp_dir == '.' or len(tmp_dir) < 1:
@@ -45,51 +46,60 @@ print(f"\nCurrent working directory: {os.getcwd()}")
 #     print(f'\n*** ERROR ***')
 #     exit(f"Can't change the Working Directory, {working_dir} doesn't exist")
 # -----------------------------------------------------------
-# - RDA files to plot
-rda_files = []
+# - pfile files to plot
+plot_files = []
 if len(sys.argv) < 2:
-    input_files =  input(f"List of files to plot RDA, **separated by space** [Default: all '.dat']: ")
+    input_files =  input(f"List of files to plot, **separated by space** [Default: all '.dat']: ")
 
     # - by default reading elements for the first XYZ file
     if len(input_files.split()) < 1:
-        for file_rda in glob.glob('*.dat'):
-            rda_files.append(file_rda)
+        for pfiles in glob.glob('*.dat'):
+            plot_files.append(pfiles)
     else:
-        rda_files = input_files.split()
+        plot_files = input_files.split()
 else:
     input_arguments = 1
     while input_arguments < len(sys.argv):
-        rda_files.append(sys.argv[input_arguments])
+        plot_files.append(sys.argv[input_arguments])
         input_arguments += 1
 
 # - checking if there is any file to plot
-if len(rda_files) > 0:
-    # print(f'\nList of files:\n\n{rda_files}\n')
+if len(plot_files) > 0:
+    # print(f'\nList of files:\n\n{plot_files}\n')
     print(f'\nList of files:\n')
-    print('\n'.join(rda_files))
-    for file_rda in rda_files:
-        if not os.path.exists(file_rda):
-            exit(f'\n*** Warinnig ***\n file {file_rda} does not exits \n')
+    print('\n'.join(plot_files))
+    for pfiles in plot_files:
+        if not os.path.exists(pfiles):
+            exit(f'\n*** Warinnig ***\n file {pfiles} does not exits \n')
 else:
     exit(f'\n *** ERROR ***\n No files found to plot\n')
 
 # -----------------------------------------------------------
 # - plotting: defining frames and designing the area to plot
+# - Stern-Limbach plot
 fig = plt.figure(figsize=(10, 8))  # inches WxH
-fig.suptitle('Distribution Analisys', fontsize=20) #, fontweight='bold')
+# fig.suptitle(f'Stern-Limbach for {transfer_list[0]}--{transfer_list[1]}$\cdots${transfer_list[2]}', fontsize=20) #, fontweight='bold')
+fig.suptitle(f'Stern-Limbach', fontsize=25) #, fontweight='bold')
 
 ax1 = plt.subplot(111)
 ax1.grid()
 
 # - legends for the main plot
-plt.ylabel('Relative Number of Ocurrences', fontsize=12) #, fontweight='bold')
-# plt.xlabel('Bond Distance [Angstrom]', fontsize=12) #, fontweight='bold')
+plt.xlabel('$q_1=(r_1-r_2)/2$ [Angstrom]', fontsize=18) #, fontweight='bold')
+plt.ylabel('$q_2=r_1+r_2$ [Angstrom]', fontsize=18) #, fontweight='bold')
+
+# -markers
+lines = ['o', '^', 'v', '<', '>', 's', 'd'] #, 'h', 'p', 'D', 'H']
+linecycler = cycle(lines)
 
 # -----------------------------------------------------------
 # - loading files to read and plot them
-for rda in rda_files:
+for pfile in plot_files:
+    name = pfile.split('/')[:-1]
+    name = '/'.join(name)
+
     x, y = [], []
-    for line in open(rda, 'r'):
+    for line in open(pfile, 'r'):
         # skipping the header
         if line.startswith("#"):
             label = [header.title() for header in line.split()]
@@ -100,42 +110,25 @@ for rda in rda_files:
         x.append(values[0])
         y.append(values[1])
 
-    # total number of distances
-    total = sum(y)
+    if pfile == 'water_hexamer/transfer_O-H-O.dat':
+        m, b = np.polyfit(x, y, 1)
+        ax1.plot(x, m*np.array(x) + b, '-', linewidth=3, color='black', label='%s' %(name))
+    else:
+        ax1.plot(x, y, next(linecycler), markerfacecolor='none', markeredgewidth=2, label='%s' %(name))
 
-    # - to smooth the curve (BSpline)
-    smooth_x = np.linspace(x[0], x[-1], len(x)*100)
+    ax1.xaxis.set_major_locator(plt.MaxNLocator(12))
 
-    # smooth curve BSpline, degree k=3, cubic
-    smooth = make_interp_spline(x, y, k=3)
-    smooth_y = smooth(smooth_x)
-
-    # - Bspline fitting
-    ax1.plot(smooth_x, smooth_y / total, label=' %s \n Total= %i' %(rda, total))
-
-    # - raw data, no Bspline fitting
-    # ax1.plot(x, y, label='%s' %rda)
-
-# - ticks for the x-axis
-# delta_x = (x[-1] - x[0]) / 10
-# ax1.xaxis.set_major_locator(plticker.MultipleLocator(base=delta_x))
-ax1.xaxis.set_major_locator(plt.MaxNLocator(12))
-
-if len(label_x) < 1:
-    label_x = input(f'Please, insert a name for x-axis: ')
-
-plt.xlabel(label_x, fontsize=12) #, fontweight='bold')
 # -----------------------------------------------------------
 # - Ending the plot
 
-# plt.legend(loc=0)
+plt.legend(loc=0, fontsize=18)
 # Put a legend below current axis
-plt.legend(loc='lower center', bbox_to_anchor=(1.32, 0.6, 0.0, 0.0),
-            fancybox=True, shadow=True, ncol=1, fontsize=11)
+# plt.legend(loc='lower center', bbox_to_anchor=(1.32, 0.6, 0.0, 0.0),
+#             fancybox=True, shadow=True, ncol=1, fontsize=11)
 
-# - Shrink current axis's height by 10% on the bottom
-box = ax1.get_position()
-ax1.set_position([box.x0, box.y0, box.width * 0.7, box.height])
+# # - Shrink current axis's height by 10% on the bottom
+# box = ax1.get_position()
+# ax1.set_position([box.x0, box.y0, box.width * 0.7, box.height])
 
 # ---------------------------------------------------------------------------------------------------------
 # - ENDING the plots
