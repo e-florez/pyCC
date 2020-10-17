1c7a034d2271b2d9107faed7a9ccdd5f459e00d9
ADanianZE <danianescobarv@gmail.com>
2020-10-16 21:47:56 -0300

    start working directory

--- functions.py
+++ functions.py
@@ -4,11 +4,13 @@
 #
 # python3.x script by:
 #
-#   name: Edison Florez (github.com/e-florez/)
-#   email: edisonffh@gmail.com
-#   affiliation: Massey University, New Zealand
+#   name        : Edison Florez (github.com/e-florez/)
+#   email       : edisonffh@gmail.com
+#   affiliation : Massey University, New Zealand
 #
-#   danianescobarv@gmail.com
+#   name        : Andy Zapata (github.com/AndyDanian)
+#   email       : danianescobarv@gmail.com
+#   affiliation : Institute of Modelling and Innovative Technology (CONICET-IMIT), Argentina
 #
 #   cesar-b29@hotmail.com
 
@@ -21,6 +23,7 @@
 # ------ Main body
 # ------------------------------------------------------------------------------------
 
+import os  # - to check id a file or dir exits -> os.path.exists()
 def working_directory():
     """
        [summary]
@@ -34,8 +37,47 @@ def working_directory():
            c ([type]): [description]
            d ([type]): [description]
     """
-    pass
 
+    """[summary]
+    Ask or get path of working directory from variable path
+    Input:
+        path (string) : with or no path of working directory
+    Output:
+        working_dir (string) : path of working directory
+    """
+    """[summary]
+    Change to working directory
+    Input:
+        working_dir (string): path of directory with .xyz
+    """
+
+
+    if len(path) <= 1 :
+        tmp_dir =  input(f'\nDirectory (whit the XYZ files) to make the RDA [default: empty]: ')
+        tmp_dir = tmp_dir.strip()
+
+        #if tmp_dir == '.' or len(tmp_dir) < 1:
+        #    working_dir = os.getcwd()
+        #else:
+        #    working_dir = os.getcwd() + '/' + tmp_dir
+    elif path[1] == '.' :
+        working_dir = os.getcwd()
+        print(f'\nWorking directiry: {working_dir}')
+    else:
+        #working_dir = os.getcwd() + '/' + path[1]
+        #La idea de pedir el path, es poner toda la dirección y
+        #en el caso de hacer la evaluación dentro de la misma
+        #carpeta que se esta parado se pone un .
+        working_dir = path[1]
+        print(f'\nWorking directiry: {working_dir}')
+
+    # Check if the working dir exists
+    if os.path.exists(working_dir) :
+        # Change the current working Directory
+        os.chdir(working_dir)
+    else:
+        print(f'\n*** ERROR ***')
+        exit(f"Can't change the Working Directory, {working_dir} doesn't exist")
 
 def reading_files_XYZ():
     """
